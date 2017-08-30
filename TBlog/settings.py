5662d83174ea9dc7f197089e4810b91fd6e627f3
Louis <louisaleksieb.dagusen@gmail.com>
2017-08-30 14:40:58 +0800

    added templates

--- TBlog/settings.py
+++ TBlog/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'TBlog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'TBlog','templates')],
+        'DIRS': [],#os.path.join(BASE_DIR, 'TBlog','templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-#STATIC_URL = '/static/'
-STATICFILES_DIRS = (
+STATIC_URL = '/static/'
+''' STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'TBlog','static'),
-)
+) '''
