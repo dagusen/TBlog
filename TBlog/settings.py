f79b07afbc91777053338c555f21ab8cb1914965
Louis <louisaleksieb.dagusen@gmail.com>
2017-09-01 12:24:02 +0800

    added navbar and carousel

--- TBlog/settings.py
+++ TBlog/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'TBlog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],#os.path.join(BASE_DIR, 'TBlog','templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'myapp','templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,6 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-''' STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'TBlog','static'),
-) '''
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'myapp','static'),
+)
--- myapp/urls.py
+++ myapp/urls.py
@@ -19,7 +19,7 @@ from . import views
 
 urlpatterns = [
     #authentication
-    url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
+    url(r'^$', auth_views.login, {'template_name': 'base.html'}, name='login'),
     url(r'^logout/$', auth_views.logout, {'next_page': 'login'}, name='logout'),
     url(r'^register/$', views.register, name='register'),
 ]
